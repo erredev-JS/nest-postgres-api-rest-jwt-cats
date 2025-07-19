cf0b866024767f51731f58c5c76d66cc4ebf09f2
erredev-JS <rodrigozapata968@gmail.com>
2025-07-19 14:00:11 -0300

    Fix y admin agregado

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,24 +1,28 @@
 import { IsString, minLength } from 'class-validator';
 import { Role } from 'src/auth/enums/rol.enum';
-import {Column, DeleteDateColumn, Entity, PrimaryGeneratedColumn} from 'typeorm'
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column()
+  name: string;
 
-    @Column()
-    name: string;
+  @Column({ unique: true, nullable: false })
+  email: string;
 
-    @Column({unique: true, nullable: false})
-    email: string;
+  @Column({ nullable: false, select: false })
+  password: string;
 
-    @Column({nullable: false, select: false})
-    password: string;
+  @Column({ default: Role.USER })
+  role: string;
 
-    @Column({default: Role.ADMIN})
-    role: string;
-
-    @DeleteDateColumn()
-    deletedAt: Date;
+  @DeleteDateColumn()
+  deletedAt: Date;
 }
