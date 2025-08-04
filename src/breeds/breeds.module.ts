4c8263488694e7f70dce9a441af9dabb0110c4c7
erredev-JS <rodrigozapata968@gmail.com>
2025-08-04 20:41:03 -0300

    Fix imports breeds.module

--- src/breeds/breeds.module.ts
+++ src/breeds/breeds.module.ts
@@ -3,9 +3,10 @@ import { BreedsService } from './breeds.service';
 import { BreedsController } from './breeds.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Breed } from './entities/breed.entity';
+import { Cat } from 'src/cats/entities/cat.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Breed])],
+  imports: [TypeOrmModule.forFeature([Breed, Cat])],
   controllers: [BreedsController],
   providers: [BreedsService],
   exports: [TypeOrmModule]
